852db9655a8ca658fcdca124b8e0533b19d86796
Wright-Fi <32863197+Wright-Fi@users.noreply.github.com>
2024-01-27 13:03:53 +0000

    Update configs.js
    Updated Meraki API from V0 to V1
    
    Added the object policyMappings - This object provides the mapping between each password and Group Policy ID.

--- configs.js
+++ configs.js
@@ -1,15 +1,23 @@
-//configs.js
-// Rename this file to configs.js 
+// configs.js
+
 // Define your Application Configurations here
 var config = {
-    // Meraki API Key
-    apiKey: "2f301bccd61b6c642d250cd3f76e5eb66ebd170f", // Demo API Key
-    // Meraki API Base URL
-    apiUrl: "https://api.meraki.com/api/v0", 
-    // The Meraki Network ID
-    networkId: "L_643451796760560141", 
-    // The Meraki Group Policy ID 
-    policy: "103"
-}
+
+  // Meraki API Key
+  apiKey: "Enter-API-Key-Here",
+
+  // Meraki API Base URL
+  apiUrl: "https://api.meraki.com/api/v1", // Updated Meraki API from V0 to V1
+
+  // The Meraki Network ID
+  networkId: "Enter-Network-ID-Here",
+
+  // Enter your password and corresponding Group Policy ID here
+  policyMappings: {
+    "Password1": "101",
+    "Password2": "102",
+    "Password3": "103"
+  }
+};
 
 module.exports = config;
